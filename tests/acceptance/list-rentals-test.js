463444bfac79b639b8b17399cc0ea27edaae1933
Lloyd Saldanha <lsaldanha@lsaldanha2019.local>
2019-10-31 09:42:30 -0400

    Init

---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,39 @@
+import { module, test } from 'qunit';
+import { visit, currentURL, click } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | list rentals', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('should show rentals as the home page', async function (assert) {
+    await visit('/');
+    assert.equal(currentURL(), '/rentals', 'Should redirect automatically');
+  });
+
+  test('should link to information about the company.', async function (assert) {
+    await visit('/');
+    await click(".menu-about");
+    assert.equal(currentURL(), '/about', 'Should navigate to about page');
+  });
+
+  test('should link to contact information.', async function (assert) {
+    await visit('/');
+    await click(".menu-contact");
+    assert.equal(currentURL(), '/contact', 'Should navigate to contact');
+  });
+
+  test('should list available rentals.', async function (assert) {
+  });
+
+  test('should filter the list of rentals by city.', async function (assert) {
+  });
+
+  test('should show details for a selected rental', async function (assert) {
+  });
+
+  // test('visiting /', async function(assert) {
+  //   await visit('/');
+
+  //   assert.equal(currentURL(), '/');
+  // });
+});
